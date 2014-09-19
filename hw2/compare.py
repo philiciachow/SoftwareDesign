61ef9f9ded3ca084af3076f0e2d7abfcba988a97
Philicia Chow <philicia.chow@gmail.com>
2014-09-18 22:55:58 -0400

    Update hw2 codes

--- hw2/compare.py
+++ hw2/compare.py
@@ -1,2 +1,13 @@
 # Soft Des HW2 
 # Think Python exercise 6-1
+
+def compare(x,y):
+	if x > y:
+		return 1
+	if x == y:
+		return 0
+	return -1
+
+print compare(2,1)
+print compare(4,4)
+print compare(5,6)
--- hw2/fermat.py
+++ hw2/fermat.py
@@ -1,2 +1,21 @@
 # Soft Des HW2 
 # Think Python exercise 5-3
+
+
+def check_fermat(a, b, c, n):
+	if n <= 2:
+		print "n must be greater than 2"
+		return
+
+	if a**n + b**n == c**n:
+		print "Holy smokes, Fermat was wrong!" 
+	else:
+		print "No, that doesn't work."
+
+
+a = int(raw_input('Enter an integer for a: '))
+b = int(raw_input('Enter an integer for b: '))
+c = int(raw_input('Enter an integer for c: '))
+n = int(raw_input('Enter an integer for n: '))
+
+check_fermat(a, b, c, n)
